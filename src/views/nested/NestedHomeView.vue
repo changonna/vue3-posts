e06d3dbb6fba5765c3647de8361d89d57a6f30e9
changon <tyu4778@gmail.com>
2025-01-20 22:04:53 +0900

    feat: 중첩 라우팅 및 404 페이지 추가

---
+++ src/views/nested/NestedHomeView.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="card">
+		<div class="card-header">Home</div>
+		<div class="card-body">
+			<h5 class="card-title">Nested home view</h5>
+			<!-- <a href="#" class="btn btn-warning">Go somewhere</a> -->
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
