63f7cad7468a177f09d874022a5118c958149723
changonna <tyu4778@gmail.com>
2025-01-30 20:48:11 +0900

    feat: AppCard 컴포넌트 추가 및 PostItem에서 사용

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,33 +1,33 @@
 <template>
-	<div class="card">
-		<div class="card-body">
-			<h5 class="card-title">{{ title }}</h5>
-			<p class="card-text">
-				{{ content }}
-			</p>
-			<p class="text-muted">{{ createdAt }}</p>
-		</div>
-	</div>
+  <AppCard>
+    <h5 class="card-title">{{ title }}</h5>
+    <p class="card-text">
+      {{ content }}
+    </p>
+    <p class="text-muted">{{ createdAt }}</p>
+  </AppCard>
 </template>
 
 <script setup>
+import AppCard from '../AppCard.vue';
+
 // import { toRef } from 'vue';
 
 const props = defineProps({
-	// post: {
-	// 	type: Object,
-	// 	required: true,
-	// },
-	title: {
-		type: String,
-		required: true,
-	},
-	content: {
-		type: String,
-	},
-	createdAt: {
-		type: [String, Date, Number],
-	},
+  // post: {
+  // 	type: Object,
+  // 	required: true,
+  // },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+  },
+  createdAt: {
+    type: [String, Date, Number],
+  },
 });
 // const { title, content, createdAt } = toRef(props.post);
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,68 +12,70 @@ import NestedTwoView from '@/views/nested/NestedTwoView.vue';
 import NestedHomeView from '@/views/nested/NestedHomeView.vue';
 
 const routes = [
-	{
-		path: '/',
-		name: 'Home',
-		component: HomeView,
-	},
-	{
-		path: '/about',
-		name: 'About',
-		component: AboutView,
-	},
-	{
-		path: '/posts',
-		name: 'PostList',
-		component: PostListView,
-	},
-	{
-		path: '/posts/create',
-		name: 'PostCreate',
-		component: PostCreateView,
-	},
-	{
-		path: '/posts/:id',
-		name: 'PostDetail',
-		component: PostDetailView,
-	},
-	{
-		path: '/posts/:id/edit',
-		name: 'PostEdit',
-		component: PostEditView,
-	},
-	{
-		path: '/:pathMatch(.*)*',
-		name: 'NotFound',
-		component: NotFoundView,
-	},
-	{
-		path: '/nested',
-		name: 'NestedView',
-		component: NestedView,
-		children: [
-			{
-				path: '',
-				name: 'NestedHome',
-				component: NestedHomeView,
-			},
-			{
-				path: 'one',
-				name: 'NestedOne',
-				component: NestedOneView,
-			},
-			{
-				path: 'two',
-				name: 'NestedTwo',
-				component: NestedTwoView,
-			},
-		],
-	},
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView,
+  },
+  {
+    path: '/posts',
+    name: 'PostList',
+    component: PostListView,
+  },
+  {
+    path: '/posts/create',
+    name: 'PostCreate',
+    component: PostCreateView,
+  },
+  {
+    path: '/posts/:id',
+    name: 'PostDetail',
+    component: PostDetailView,
+    // props: true,
+    props: route => ({ id: route.params.id }),
+  },
+  {
+    path: '/posts/:id/edit',
+    name: 'PostEdit',
+    component: PostEditView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundView,
+  },
+  {
+    path: '/nested',
+    name: 'NestedView',
+    component: NestedView,
+    children: [
+      {
+        path: '',
+        name: 'NestedHome',
+        component: NestedHomeView,
+      },
+      {
+        path: 'one',
+        name: 'NestedOne',
+        component: NestedOneView,
+      },
+      {
+        path: 'two',
+        name: 'NestedTwo',
+        component: NestedTwoView,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory('/'),
-	routes,
+  history: createWebHistory('/'),
+  routes,
 });
 
 export default router;
