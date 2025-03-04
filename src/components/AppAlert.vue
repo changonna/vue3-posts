f52b3b7cdbf9c43d0e5ba45dee35973b3628860d
changon <tyu4778@gmail.com>
2025-03-04 22:19:45 +0900

    Feat: (Transition) AppAlert 컴포넌트에 슬라이드 전환 효과 추가

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,12 +1,14 @@
 <template>
-  <div
-    v-if="isVisible"
-    class="alert-area alert"
-    :class="alertClass"
-    role="alert"
-  >
-    {{ msg }}
-  </div>
+  <Transition name="slide">
+    <div
+      v-if="isVisible"
+      class="alert-area alert"
+      :class="alertClass"
+      role="alert"
+    >
+      {{ msg }}
+    </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -27,5 +29,20 @@ const alertClass = computed(() => {
   position: fixed;
   top: 30px;
   right: 10px;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 1s;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
 }
 </style>
